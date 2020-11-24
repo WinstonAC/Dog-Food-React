03b3b63bad3206bdfeed0ddab69a5bd1565d6064
William <Billy.campbell17@gmail.com>
2020-11-24 16:17:31 -0500

    updated

---
+++ src/Components/Button.js
@@ -0,0 +1,13 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import Button from "@material-ui/core/Button";
+
+function Button1() {
+  return (
+    <Button variant="contained" color="primary">
+      WOOF
+    </Button>
+  );
+}
+
+export default Button1;
